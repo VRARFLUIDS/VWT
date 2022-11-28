fd9ed25d748cb1d959bc3204506c6710946746aa
VRARFLUIDS <119084795+VRARFLUIDS@users.noreply.github.com>
2022-11-28 19:35:07 -0400

    Add files via upload
    Files correspond to VWT. Contains flow animation scripts, color map image and GLTF example files.

---
+++ AnimationMeshesMap.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class AnimationMeshesMap : MonoBehaviour
+{
+    public string GLTFFolderPath = "Assets/Resources/GLTFs/";
+    public string GLTFGeneralName = "PUVWT_1-565.32";
+    public int GLTFFirstFrame = 6000;
+    public string GLTFextension = ".gltf";
+    public int GLTFNameIncrementer= 10;
+    public int NumberOfFrames = 11;
+    public bool FillMeshesList = true;
+    public bool ForceRefillList = false;
+    
+    public Mesh[] MeshesList;
+
+    void Awake()
+    {
+
+        if (FillMeshesList)
+        {
+            if (IsReady()) 
+            {
+                if (ForceRefillList)
+                {
+                    Debug.Log("It was ready but now refilling meshes list");
+                    MeshesPointer();
+                    return;
+                }
+                Debug.Log("It was ready and meshes list was not modified");
+                return;
+            }
+            Debug.Log("It was not ready but now refilling meshes list");
+            MeshesPointer();
+         }
+    }
+
+    bool IsReady()
+    {
+        if (MeshesList.Length > 0)
+            return true;
+        Debug.LogError("No Meshes have been added");
+        return false;
+    }
+
+    void MeshesPointer()
+    {
+        MeshesList = new Mesh[NumberOfFrames];
+
+        for (int i = 0; i < NumberOfFrames; i++)
+        {
+            int GLTFFrame = (GLTFFirstFrame + GLTFNameIncrementer*i);
+            string GLTFCompleteName = string.Concat(GLTFFolderPath, GLTFGeneralName, GLTFFrame.ToString("0000"),GLTFextension);
+            Debug.Log("Looking for Mesh of file: " + GLTFCompleteName);
+        
+            Object[] data = AssetDatabase.LoadAllAssetsAtPath(GLTFCompleteName);
+            foreach (Object x in data)
+            {
+                if (x.name == "mesh1" && x is Mesh)
+                {
+                    Debug.Log("File found");
+                    MeshesList[i]= x as Mesh;
+                    break;
+                }  
+            }
+        }
+    }
+}
---
+++ FlowAnimator.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class FlowAnimator : MonoBehaviour
+{
+    AnimationMeshesMap animationMeshesMap;
+
+    public float FramesPerSecond = 12;
+    public bool PlayAnimation = true;
+    public Material MaterialToUse;
+
+    Mesh[] Meshes;
+    protected MeshFilter MyMeshFilter;
+    protected int FrameIndx = 0;
+    
+    public bool IsPlaying { get; private set; }
+
+    void Start()
+    {
+        IsPlaying = false;
+
+        animationMeshesMap = GetComponent<AnimationMeshesMap>();
+        Meshes = animationMeshesMap.MeshesList;
+
+        if (!CheckIfReady()) return;
+        MyMeshFilter = GetComponent<MeshFilter>();
+        MyMeshFilter.mesh = Meshes[0];
+        GetComponent<MeshRenderer>().material = MaterialToUse;
+
+        if (PlayAnimation)
+            Play();
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(IsPlaying)
+                Stop();
+            else
+                Play();
+        }
+    }
+
+    public void Play()
+    {
+        if (!CheckIfReady()) return;
+
+        if (!IsPlaying)
+        {
+            IsPlaying = true;
+            StartCoroutine(AnimateMesh());
+        }
+    }
+
+    public void Stop()
+    {
+        if (!CheckIfReady()) return;
+
+        if (IsPlaying)
+        {
+            IsPlaying = false;
+            StopCoroutine("AnimateMesh");
+        }
+    }
+
+    IEnumerator AnimateMesh()
+    {
+        var waitTime = 1/FramesPerSecond;
+
+        while (IsPlaying)
+        {
+            if (FrameIndx > Meshes.Length - 1)
+                FrameIndx = 0;
+
+            MyMeshFilter.mesh = Meshes[FrameIndx];
+            Debug.Log("Mesh index: " + FrameIndx );
+            FrameIndx++;
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+
+    bool CheckIfReady()
+    {
+        if (Meshes.Length > 0)
+            return true;
+        Debug.LogError("No Meshes have been added to the Method1A");
+        return false;
+    }
+}
